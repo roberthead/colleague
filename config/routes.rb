afd6cdeaead15bf6ee7f3405704ee66e4290d3f9
Rob Head <robert.head@gmail.com>
2023-05-02 12:43:22 -0400

    Name on landing page.
    - first specs

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resource :landing, only: :show
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root "landings#show"
 end
---
+++ spec/system/landing_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe "Landing", type: :system do
+  before do
+    driven_by(:rack_test)
+  end
+
+  scenario "visiting the root url" do
+    visit '/'
+
+    expect(page).to have_text("Robert Emerson Head")
+  end
+end
