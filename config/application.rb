6ea52107aaef9e8142e7e3b3074bdf21fface5dc
Rob Head <robert.head@gmail.com>
2023-05-12 11:04:43 -0400

    Don't generate helper files.

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,8 @@ module Colleague
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    # Don't generate helper files
+    config.generators.helper = false
   end
 end
